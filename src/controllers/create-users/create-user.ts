1e3efeaa2463e9ea2e2eb993c6d46e6b153ad500
Camilla <milla_mendes83@hotmail.com>
2025-01-06 00:15:57 -0300

    refactor: :recycle: add controller helper functions

--- src/controllers/create-users/create-user.ts
+++ src/controllers/create-users/create-user.ts
@@ -1,48 +1,38 @@
 import validator from "validator";
+
 import { User } from "../../models/user";
-import { HttpResponse, HttpRequest, IController } from "../protocols";
+import { badRequest, created, serverError } from "../helpers";
+import { HttpRequest, HttpResponse, IController } from "../protocols";
 import { CreateUserParams, ICreateUserRepository } from "./protocols";
 
 export class CreateUserController implements IController {
   constructor(private readonly createUserRepository: ICreateUserRepository) { }
 
-  async handle(httpRequest: HttpRequest<CreateUserParams>): Promise<HttpResponse<User | string>> {
+  async handle(
+    httpRequest: HttpRequest<CreateUserParams>
+  ): Promise<HttpResponse<User | string>> {
     try {
-
-      // verificar campos obrigatorios
       const requiredFields = ["firstName", "lastName", "email", "password"];
 
       for (const field of requiredFields) {
         if (!httpRequest?.body?.[field as keyof CreateUserParams]?.length) {
-          return {
-            statusCode: 400,
-            body: `Field ${field} is required.`,
-          };
+          return badRequest(`Field ${field} is required`);
         }
       }
 
-      // verificar se o email é válido
       const emailIsValid = validator.isEmail(httpRequest.body!.email);
 
       if (!emailIsValid) {
-        return {
-          statusCode: 400,
-          body: "Email is invalid"
-        }
+        return badRequest("E-mail is invalid");
       }
 
-      const user = await this.createUserRepository.createUser(httpRequest.body!);
+      const user = await this.createUserRepository.createUser(
+        httpRequest.body!
+      );
 
-      return {
-        statusCode: 201,
-        body: user,
-      };
+      return created<User>(user);
     } catch (error) {
-      console.error("Error in CreateUserController:", error);
-      return {
-        statusCode: 500,
-        body: "something went wrong."
-      };
+      return serverError();
     }
   }
 }
