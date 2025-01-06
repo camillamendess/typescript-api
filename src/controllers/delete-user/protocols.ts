d3e02f833c072b1c466bfadd919dde3f935ea1a6
Camilla <milla_mendes83@hotmail.com>
2025-01-05 23:50:18 -0300

    feat: :sparkles: add DeleteUserController and its route

--- src/controllers/delete-user/protocols.ts
+++ src/controllers/delete-user/protocols.ts
@@ -1,4 +1,9 @@
 import { User } from "../../models/user";
+import { HttpResponse, HttpResquest } from "../protocols";
+
+export interface IDeleteUserController {
+  handle(httpRequest: HttpResquest<any>): Promise<HttpResponse<User>>;
+}
 
 export interface IDeleteUserRepository {
   deleteUser(id: string): Promise<User>;
